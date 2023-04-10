53c67abf3f0a5b7b466e8d717a4ef390bd9aab69
JEROMEREYNANTE30160763 <113806026+JEROMEREYNANTE30160763@users.noreply.github.com>
2023-04-09 21:30:28 -0600

    Finalized Documentation

--- src/main/java/mylib/datastructures/trees/AVL.java
+++ src/main/java/mylib/datastructures/trees/AVL.java
@@ -8,14 +8,27 @@ import main.java.mylib.datastructures.nodes.TNode;
 public class AVL extends BST {
     private TNode root;
 
+    /**
+     * Default constructor
+     */
     public AVL() {
         this.root = null;
     }
 
+    /**
+     * Overload constructor
+     * 
+     * @param val the data of the root node
+     */
     public AVL(int val) {
-        this.root = new TNode(val,0,null,null,null);
+        this.root = new TNode(val, 0, null, null, null);
     }
 
+    /**
+     * Overload constructor
+     * 
+     * @param obj the root node
+     */
     public AVL(TNode obj) {
         if (obj == null) {
             this.root = null;
@@ -25,6 +38,11 @@ public class AVL extends BST {
         }
     }
 
+    /**
+     * setter for the root
+     * 
+     * @param root the root node
+     */
     public void setRoot(TNode root) {
         this.root = root;
         if (root != null) {
@@ -32,22 +50,42 @@ public class AVL extends BST {
         }
     }
 
+    /**
+     * getter for the root
+     * 
+     * @return the root node
+     */
     public TNode getRoot() {
         return this.root;
     }
 
+    /**
+     * insert a data into the tree
+     * 
+     * @param val the data of the node to be inserted
+     */
     @Override
     public void insert(int val) {
         super.insert(val);
         balanceTree();
     }
 
+    /**
+     * insert a node into the tree
+     * 
+     * @param node the node to be inserted
+     */
     @Override
     public void insert(TNode node) {
         super.insert(node);
         balanceTree();
     }
 
+    /**
+     * delete a node from the tree
+     * 
+     * @param val the data of the node to be deleted
+     */
     @Override
     public void delete(int val) {
         TNode nodeToDelete = search(val);
@@ -59,12 +97,21 @@ public class AVL extends BST {
         balanceTree();
     }
 
+    /**
+     * balance the tree
+     */
     private void balanceTree() {
         if (root != null) {
             root = balanceTree(root);
         }
     }
 
+    /**
+     * balance the tree
+     * 
+     * @param node the node to be balanced
+     * @return the balanced node
+     */
     private TNode balanceTree(TNode node) {
         if (node == null) {
             return null;
@@ -88,6 +135,12 @@ public class AVL extends BST {
         return node;
     }
 
+    /**
+     * rotate the tree to the left
+     * 
+     * @param node
+     * @return
+     */
     private TNode rotateLeft(TNode node) {
         TNode newRoot = node.getRight();
         node.setRight(newRoot.getLeft());
@@ -97,6 +150,12 @@ public class AVL extends BST {
         return newRoot;
     }
 
+    /**
+     * rotate the tree to the right
+     * 
+     * @param node
+     * @return
+     */
     private TNode rotateRight(TNode node) {
         TNode newRoot = node.getLeft();
         node.setLeft(newRoot.getRight());
@@ -106,6 +165,12 @@ public class AVL extends BST {
         return newRoot;
     }
 
+    /**
+     * get the height of the node
+     * 
+     * @param node
+     * @return
+     */
     private int getHeight(TNode node) {
         if (node == null) {
             return -1;
@@ -114,6 +179,11 @@ public class AVL extends BST {
         }
     }
 
+    /**
+     * update the height of the node
+     * 
+     * @param node
+     */
     private void updateHeight(TNode node) {
         if (node != null) {
             node.setBalance(Math.max(getHeight(node.getLeft()), getHeight(node.getRight())) + 1);
--- src/main/java/mylib/datastructures/trees/BST.java
+++ src/main/java/mylib/datastructures/trees/BST.java
@@ -11,27 +11,56 @@ import java.util.Queue;
 public class BST {
     private TNode root;
 
+    /**
+     * Default constructor
+     */
     public BST() {
         root = null;
     }
 
+    /**
+     * Overload constructor
+     * 
+     * @param val the data of the root node
+     */
     public BST(int val) {
-        this.root = new TNode(val,0,null,null,null);
+        this.root = new TNode(val, 0, null, null, null);
     }
 
+    /**
+     * Overload constructor
+     * 
+     * @param obj the root node
+     */
     public BST(TNode obj) {
         this.root = obj;
     }
 
+    /**
+     * setter for the root
+     * 
+     * @param root the root node
+     */
     public void setRoot(TNode root) {
         this.root = root;
     }
 
+    /**
+     * getter for the root
+     * 
+     * @return the root node
+     */
     public TNode getRoot() {
         return root;
     }
+
+    /**
+     * insert a data into the tree
+     * 
+     * @param val the data to be inserted
+     */
     public void insert(int val) {
-        TNode newNode = new TNode(val,0,null,null,null);
+        TNode newNode = new TNode(val, 0, null, null, null);
         if (root == null) {
             root = newNode;
         } else {
@@ -39,6 +68,12 @@ public class BST {
         }
     }
 
+    /**
+     * insert a node into the tree
+     * 
+     * @param node    the root of the tree
+     * @param newNode the node to be inserted
+     */
     private void insert(TNode node, TNode newNode) {
         if (newNode.getData() < node.getData()) {
             if (node.getLeft() == null) {
@@ -55,6 +90,11 @@ public class BST {
         }
     }
 
+    /**
+     * insert a node into the tree
+     * 
+     * @param node the node to be inserted
+     */
     public void insert(TNode node) {
         if (root == null) {
             root = node;
@@ -63,10 +103,22 @@ public class BST {
         }
     }
 
+    /**
+     * delete a node from the tree
+     * 
+     * @param val the value of the node to be deleted
+     */
     public void delete(int val) {
         root = delete(root, val);
     }
 
+    /**
+     * delete a node from the tree
+     * 
+     * @param node the root of the tree
+     * @param val  the value of the node to be deleted
+     * @return the node that is deleted
+     */
     private TNode delete(TNode node, int val) {
         if (node == null) {
             System.out.println("Value " + val + " is not in the tree.");
@@ -92,6 +144,12 @@ public class BST {
         return node;
     }
 
+    /**
+     * find the minimum node in the tree
+     * 
+     * @param node the root of the tree
+     * @return the minimum node
+     */
     private TNode findMin(TNode node) {
         while (node.getLeft() != null) {
             node = node.getLeft();
@@ -99,10 +157,23 @@ public class BST {
         return node;
     }
 
+    /**
+     * search a node in the tree
+     * 
+     * @param val the value of the node to be searched
+     * @return the node that is searched
+     */
     public TNode search(int val) {
         return search(root, val);
     }
 
+    /**
+     * search a node in the tree
+     * 
+     * @param node the root of the tree
+     * @param val  the value of the node to be searched
+     * @return the node that is searched
+     */
     private TNode search(TNode node, int val) {
         if (node == null || node.getData() == val) {
             return node;
@@ -114,11 +185,19 @@ public class BST {
         }
     }
 
+    /**
+     * print the tree in order
+     */
     public void printInOrder() {
         printInOrder(root);
         System.out.println();
     }
 
+    /**
+     * print the tree in order
+     * 
+     * @param node the root of the tree
+     */
     private void printInOrder(TNode node) {
         if (node != null) {
             printInOrder(node.getLeft());
@@ -126,6 +205,10 @@ public class BST {
             printInOrder(node.getRight());
         }
     }
+
+    /**
+     * print the tree in BF
+     */
     public void printBF() {
         if (root == null) {
             return;
